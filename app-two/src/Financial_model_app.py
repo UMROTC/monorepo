6532e1b5aa3ab3af35f9419fd2337f10c25ffd65
Jack Helmsing <jack.helmsing@gmail.com>
2025-01-23 07:56:39 -0700

    changed plotly to plotly express

--- app-two/src/Financial_model_app.py
+++ app-two/src/Financial_model_app.py
@@ -8,7 +8,7 @@ Monthly Bar Chart Race for Net Worth
 
 import pandas as pd
 import numpy as np
-import plotly as px
+import plotly.express as px
 import os
 
 # -------------------------------------------------------------------------
