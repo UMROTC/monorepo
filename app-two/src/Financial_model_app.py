b70da6bf2bf0bd55f2c49af474a9e2be9aa8bb0b
Jack Helmsing <jack.helmsing@gmail.com>
2025-01-25 09:27:36 -0700

    changed code in financial model to say plotly only

--- app-two/src/Financial_model_app.py
+++ app-two/src/Financial_model_app.py
@@ -8,7 +8,7 @@ Monthly Bar Chart Race for Net Worth
 
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly as px
 import os
 
 # -------------------------------------------------------------------------
