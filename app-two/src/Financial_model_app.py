5830a3b6f863cc3f5243d8eb62c3bb34847d006d
Jack Helmsing <jack.helmsing@gmail.com>
2025-03-12 14:23:42 -0600

    asdf

--- app-two/src/Financial_model_app.py
+++ app-two/src/Financial_model_app.py
@@ -548,7 +548,7 @@ def generate_pair_report(c_row, m_row):
         yanchor="top",
         y=1.02,
         xanchor="left",
-        x=5
+        x=-2
     ))
     chart_html = get_chart_image(chart_fig)
     
